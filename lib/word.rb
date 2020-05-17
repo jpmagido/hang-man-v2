9a8430761c2121e9961eec7120268ef885c95a15
jpmagido <jpmagido@me.com>
2020-05-17 22:28:47 +0200

    add spec crypted word

--- lib/word.rb
+++ lib/word.rb
@@ -1,10 +1,30 @@
 require 'ffaker'
 
 class Word
-  attr_accessor :length, :word
+  attr_accessor :length, :word, :crypted
 
-  def initialize
-    @word = FFaker::LoremFR.words(10)
+  def initialize(level = 1)
+    @word = word_difficulty(level)
     @length = @word.length
+    @crypted = to_crypt(@word)
+  end
+
+  private
+
+  def word_difficulty(input)
+    case
+    when input == 1
+      FFaker::LoremFR.words(50).detect { |word| word.length == 5 }
+    when input == 2
+      FFaker::LoremFR.words(50).detect { |word| word.length == 7 }
+    else
+      FFaker::LoremFR.words(50).detect { |word| word.length >= 9 }
+    end
+  end
+
+  def to_crypt(input)
+    encrypted_word = {}
+    input.chars.map.with_index { |l, i| encrypted_word[i] = [l, '_'] }
+    encrypted_word
   end
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -1,6 +1,9 @@
 require './lib/word.rb'
 
 RSpec.describe Word do
+  let(:word_level_1) { Word.new(1) }
+  let(:word_level_2) { Word.new(2) }
+  let(:word_level_3) { Word.new(3) }
 
   subject { Word.new }
 
@@ -8,8 +11,27 @@ RSpec.describe Word do
     it 'should create a word with proper attributes' do
       subject
       expect(subject.instance_variable_get(:@word)).not_to be_nil
+      expect(subject.word.class).to eq(String)
+      expect(subject.instance_variable_get(:@word).class).to eq(String)
       expect(subject.instance_variable_get(:@word)).not_to be_empty
       expect(subject.instance_variable_get(:@length)).to eq(subject.word.length)
+    end
+
+    it 'should create a word based on difficulty' do
+      expect(word_level_1.length).to be <= 5
+      expect(word_level_2.length).to eq 7
+      expect(word_level_3.length).to be >= 9
+    end
+
+    it 'should have a crypted format' do
+      expect(subject.instance_variable_get(:@crypted)).not_to be_nil
+      expect(subject.instance_variable_get(:@crypted).class).to eq(Hash)
+    end
+
+    it 'should encrypt the word' do
+      expect(subject.crypted.values.map(&:first).join).to eq(subject.word)
+      expect(subject.crypted.values.map(&:last).join).to eq('_' * subject.word.length)
+      expect(subject.crypted.length).to eq(subject.word.length)
     end
   end
 end
